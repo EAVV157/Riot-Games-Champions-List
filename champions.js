9616a48cc9fc7a7d956e7fcac2f145c04dbe43ba
Edgar <edvargas1817@gmail.com>
2025-03-23 22:31:00 -0600

    Various changes to HTML, CSS and JS. Removing unecessary code.

--- champions.js
+++ champions.js
@@ -1,129 +1,95 @@
-var version = "15.5.1";
+var searchTag = "";
+var searchChampName = "";
 
-/* ----- Set search bar values from index/local storage ----- */
-var role = document.getElementById("role").value;
-var champName = document.getElementById("champ-name").value;
-
-/* ----- Same function from Index ----- */
-function storeSummonerInfo() {
-  gameName = fullGameName.match(/\b(?<!#)[^#\s]+/g);
-  tagLine = fullGameName.match(/(?<=#)\w+/gm);
+/* ----- Set search bar values  ----- */
+function searchChampions(){
+  searchTag = document.getElementById("role").value;
+  searchChampName = document.getElementById("champ-name").value;
+  console.log(searchChampName + ", " + searchTag);
 }
-
-class ChampionRoster {
-  constructor(id, level, key, name, title) {
-    this.id = id;
-    this.level = level;
-    this.key = key;
-    this.name = name;
-    this.title = title;
-  }
-
-  setChampId(id) {
-    this.id = id;
-  }
-  setChampLevel(level) {
-    this.level = level;
-  }
-  setChampKey(key) {
-    this.key = key;
-  }
-  setChampName(name) {
-    this.name = name;
-  }
-  setChampTitle(title) {
-    this.title = title;
-  }
-
-  getChampId() {
-    return this.id;
-  }
-  getChampLevel() {
-    return this.level;
-  }
-  getChampKey() {
-    return this.key;
-  }
-  getChampName() {
-    return this.name;
-  }
-  getChampTitle() {
-    return this.title;
-  }
-}
-
-/* Change this later */ var filter = 3;
 
 /* ----- API DATA AQUISITIONS ----- */
-async function renderAPIs() {
-  /* API to obtain the latest version of the game */
+async function getVersion() {
   const apiVersion = await fetch(
     `https://ddragon.leagueoflegends.com/api/versions.json`
   );
   const apiVersionData = await apiVersion.json();
   version = apiVersionData[0];
-  console.log(version);
+  return version;
+}
 
-  /* API to obtain the names of the champions that exist in the game */
+async function getChampions(version) {
   const championListLive = await fetch(
     `https://ddragon.leagueoflegends.com/cdn/${version}/data/en_US/champion.json`
   );
   const championListLiveData = await championListLive.json();
   const champInfo = championListLiveData.data;
-
-  let championsLive = new Map();
-  for (var key in champInfo) {
-    let tempName = champInfo[key].name;
-    let tempId = champInfo[key].key;
-    let tempTitle = champInfo[key].title;
-    /* console.log(tempName + ": " + tempId + "," + tempTitle); */
-
-    dynamicChamps.set(tempId, tempName);
-
-    for (var k in champInfo[key]) {
-      console.log(`${k}: ${champInfo[key][k]}`)
-    }
-  }
-
-  /* ----- Remove the Loading HTML from the webpage ----- */
-  const summonerLoading = document.querySelector(".summoner");
-  summonerLoading.classList.remove("summoner__loading");
-
-  /* HTML TO CREATE THE SUMMONER PROFILE (Summoner.html has it) */
-  /* `<div class="summoner__profile">
-            <div class="summoner__profile--display">
-              <div class="summoner__profile--wrapper">
-                <img
-                  src="https://ddragon.leagueoflegends.com/cdn/${version}/img/profileicon/${iconId}.png"
-                  class="summoner__profile--img"
-                />
-              </div>
-              <h1 class="summoner__profile--name">${gameName} <span class="silver">#${tagLine}</span></h1>
-            </div>
-            <div class="summoner__profile--filter">
-              <h3 class="summoner__profile--filter-sub">Filter:</h3>
-              <select id="filter" class="summoner__filter" onchange="filterChamps(event)">
-                <option value="TOP_3" selected>Top 3 Champions</option>
-                <option value="TOP_5">Top 5 Champions</option>
-                <option value="TOP_10">Top 10 Champions</option>
-                <option value="A_Z_10">Alphabetical Top 10</option>
-              </select>
-            </div>
-          </div>` */
+  return champInfo;
 }
 
+/* ----- MAIN ASYNC LOADING FUNCTION ----- */
+async function renderChampions() {
+  const currentVersion = await getVersion();
+  console.log(currentVersion);
+  const existingChampions = await getChampions(currentVersion);
+  console.log(existingChampions);
+
+  for (var key in existingChampions) {
+    let tempName = existingChampions[key].name;
+    let tempId = existingChampions[key].key;
+    let tempTitle = existingChampions[key].title;
+    let tempTags = existingChampions[key].tags;
+    console.log("(" + key.toLowerCase() + ") "+ tempName + ": " + tempId + ", " + tempTitle + ", " + tempTags.join('/'));
 
 
-/* var championsLive = [];
-function getChamps(champList) {
-    return new Promise((resolve, reject) => {
+  }
+
+  
+
+  /* if (role === "Assassin") {
+    champs.sort(
+      (a, b) =>
         
-    })
-} */
+    );
+  }
+  else if(role === "All") {
+    champs.sort(
+      (a, b) => a.name
+    );
+  } */
+}
 
+/* ----- PAGE LOADING TIMERS ----- */
+const fadeOutTime = 2500;
+/* --- Preview Fadeout --- */
 setTimeout(() => {
-  renderAPIs();
-}, Math.random() * 1500 + 2500);
+  const preview = document.querySelector(".champions__preview");
+  preview.style.transition = 'opacity 1s';
+  preview.style.opacity = 0;
+
+  setTimeout(() => {
+    preview.style.display = 'none';
+  }, 750);
+
+}, fadeOutTime);
+/* --- Champions Fadein */
+setTimeout(() => {
+  renderChampions();
+}, Math.random() * 2000 + fadeOutTime);
+
+
+
+
+
+
+
+
+
+
+
+/* ----- Remove the Loading HTML from the webpage ----- */
+/* const summonerLoading = document.querySelector(".summoner");
+summonerLoading.classList.remove("summoner__loading"); */
 
 async function renderChampions1(status, filter) {
   var topChampsAmmount = filter;
